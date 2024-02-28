db5f57887ed6828fa37ef2aa31d366a8ca83101d
Paridhi Arya <114894614+paridhiarya@users.noreply.github.com>
2024-02-28 19:42:11 +0530

    Add files via upload

---
+++ face_detection.py
@@ -0,0 +1,22 @@
+import cv2
+
+cascade_face = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, img = cap.read()
+    g = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    f = cascade_face.detectMultiScale(g, 1.3, 5)
+    
+    for (x, y, w, h) in f:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 4)
+    
+    cv2.imshow('Image', img)
+    k = cv2.waitKey(30) & 0xff
+    
+    if k == 27:
+        break
+    
+cap.release()
+cv2.destroyAllWindows()
